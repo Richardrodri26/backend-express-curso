78311b16bdee462ebdd0ee9c7f9d860b8f0fe7e9
Richardrodri26 <richardmanuel811@gmail.com>
2023-02-21 15:45:11 -0500

    changes in db/config.js

--- config/config.js
+++ config/config.js
@@ -4,16 +4,16 @@ const config = {
   env:process.env.NODE_ENV || 'dev',
   isProd:process.env.NODE_ENV ==='production',
   port:process.env.PORT || 3000,
-  dbUSer:process.env.DB_USER,
-  dbPassword:process.env.DB_PASSWORD,
-  dbHost:process.env.DB_HOST,
-  dbName:process.env.DB_NAME,
-  dbPort:process.env.DB_PORT,
+  dbUrl:process.env.DATABASE_URL,
+  //dbUSer:process.env.DB_USER,
+  //dbPassword:process.env.DB_PASSWORD,
+  //dbHost:process.env.DB_HOST,
+  //dbName:process.env.DB_NAME,
+  //dbPort:process.env.DB_PORT,
   apiKey:process.env.API_KEY,
   jwtSecret:process.env.JWT_SECRET,
   userMail:process.env.USER_MAIL,
   passwordMail:process.env.PASSWORD_MAIL,
-  dbUrl:process.env.DATABASE_URL,
 
 }
 
